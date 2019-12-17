f1a41aaa91a221a7ee136884760d4d5399657aea
TechDev40 <Techdev40@gmail.com>
2019-12-17 14:15:02 +0200

    Update the layout

--- app/src/main/java/com/android/academy/DetailsActivity.kt
+++ app/src/main/java/com/android/academy/DetailsActivity.kt
@@ -1,12 +1,21 @@
 package com.android.academy
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class DetailsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        details_btn_trailer2.setOnClickListener {
+            val trailerUrl = getString(R.string.trailerUrl)
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(trailerUrl))
+            startActivity(browserIntent)
+        }
     }
 }
